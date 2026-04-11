09eaa8a4b68c8e5e95175d40e03b6eedb9c076c0
Julius-Z12 <julius.zemeckis@gmail.com>
2026-04-11 10:32:06 +0300

    BLABLA

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var bouncables = [];
 function setup() {
   createCanvas(400, 400);
 
-  for (var i = 0; i < 10000; i++) {
+  for (var i = 0; i < 10; i++) {
     bouncables.push(new Bouncable(
       color(random(0, 255), random(0, 255), random(0, 255)),
       random(50, 350), 
